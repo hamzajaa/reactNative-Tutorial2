5da6f55fdc42f03affccd2d035c3cc08f1f1f38c
hamzajaa <hamzajaa2017@gmail.com>
2023-08-17 22:55:32 +0100

    change 2.0

--- App.js
+++ App.js
@@ -4,14 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.blueFont}>Open up App.js to start working on your app!</Text>
-      <Text style={{ color: '#0f0' }}>Open up App.js to start working on your app!</Text>
-      <Text style={styles.blueFont}>Open up App.js to start working on your app!</Text>
-      <View>
-        <Text>fffda</Text>
-        <Text>fffda</Text>
+      <View style={styles.myStyle}>
+        <Text style={styles.blueFont}></Text>
       </View>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -19,11 +14,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  blueFont: {
-    color: '#00f'
-  }
+  myStyle: {
+    width: "100%", // 100% from parents Component
+    height: 50,
+    backgroundColor: "#F00",
+  },
 });

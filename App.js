5b9418fe90696745202eac295f8a073a35f6b5be
hamzajaa <hamzajaa2017@gmail.com>
2023-08-17 23:41:10 +0100

    change 8.0

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <View style={styles.viewStyle1}></View>
       <View style={styles.viewStyle2}></View>
+      <View style={styles.viewStyle3}></View>
     </View>
   );
 }
@@ -13,20 +14,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    alignItems: "flex-end",
   },
   viewStyle1: {
     width: 100,
     height: 100,
-    backgroundColor: "red",
-    alignSelf: "flex-start",
+    backgroundColor: "#F00",
+    position: 'relative',
   },
   viewStyle2: {
     width: 100,
     height: 100,
-    backgroundColor: "blue"
+    backgroundColor: "#00F",
+    position: 'relative',
+  },
+  viewStyle3: {
+    width: 100,
+    height: 100,
+    backgroundColor: "#0F0",
+    position: 'absolute',
+    top:50,
+    left: 50,
   },
 
 });

724576ba0b84d32a29164d602f92418af6d7a9af
hamzajaa <hamzajaa2017@gmail.com>
2023-08-17 23:56:52 +0100

    change 10.0

--- App.js
+++ App.js
@@ -2,15 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-paper';
 
+const MyView = () => {
+  return (
+    <View style={{ width: 100, height: 100, backgroundColor: "red" }}></View>
+  )
+}
 export default function App() {
+  let v = "Text";
   return (
     <View style={styles.container}>
-      <Button
-        icon="camera"
-        mode="contained"
-        onPress={() => console.log('Pressed')}>
-        Press me
-      </Button>
+      <MyView />
+      <Text style={{ margin: 100 }}>{v}</Text>
     </View>
   );
 }

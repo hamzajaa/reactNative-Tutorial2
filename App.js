a7b29438acec0cedc5440bd2d592efc58c42506e
hamzajaa <hamzajaa2017@gmail.com>
2023-08-18 00:01:08 +0100

    change 11.0

--- App.js
+++ App.js
@@ -2,17 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-paper';
 
-const MyView = () => {
-  return (
-    <View style={{ width: 100, height: 100, backgroundColor: "red" }}></View>
-  )
-}
+
 export default function App() {
   let v = "Text";
   return (
     <View style={styles.container}>
-      <MyView />
-      <Text style={{ margin: 100 }}>{v}</Text>
+      <View style={{ backgroundColor: "red", width: 100, height: 100 }}>
+        <Text style={{margin: 20, backgroundColor: "#0f0"}}>Sample Text</Text>
+      </View>
     </View>
   );
 }

802c98143e2012d15a111ca9cc9242414a578937
hamzajaa <hamzajaa2017@gmail.com>
2023-08-18 00:02:05 +0100

    change 11.1

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-paper';
-
 
 export default function App() {
-  let v = "Text";
   return (
     <View style={styles.container}>
       <View style={{ backgroundColor: "red", width: 100, height: 100 }}>
